40c65d2e4595c03db07dbef80d5264f963a7ad59
Tubomi <41150141+Tubomi@users.noreply.github.com>
2018-12-07 15:05:48 +0800

    Update 替换汇总.py

--- 替换汇总.py
+++ 替换汇总.py
@@ -12,7 +12,7 @@ total=np.sum([din['计费数'],din['续订数']],axis=0)
 din['后台计费数']=total
 ping=din[din['Offer'].isin(['Y1','Y2'])]
 ping=ping[['时间','Offer','运营商','订阅数','后台计费数']]
-ping=ping[ping['运营商'].isin(['Tim','Wind'])]
+ping=ping[ping['运营商'].isin(['Tim','Wind'])] #isin步骤应该还能再优化
 ping=ping.groupby(['Offer','运营商']).sum()
 ping.to_excel('E:\\2018\\订阅\\二月订阅\\123.xls',\
                   sheet_name='sheet0')
